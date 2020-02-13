b7a1444725a08f9f96ca2dade85d2ee9819f0d54
Matt Roberts <mattjr72@gmail.com>
2020-02-13 10:21:49 +0000

    unit tell for play interpreter

--- interpreter-play/shared/src/main/scala/PlayTwirlController.scala
+++ interpreter-play/shared/src/main/scala/PlayTwirlController.scala
@@ -1,6 +1,7 @@
 package ltbs.uniform
 package interpreters.playframework
 
+import ltbs.uniform.common.web.GenericWebTell
 import play.twirl.api.Html
 import play.api._,mvc._
 import concurrent.ExecutionContext
@@ -8,5 +9,9 @@ import concurrent.ExecutionContext
 abstract class PlayTwirlInterpreter(controller: Results)(
   implicit ec: ExecutionContext
 ) extends PlayInterpreter[Html](controller) {
+
+  implicit def unitTell: GenericWebTell[Unit, Html] = new GenericWebTell[Unit, Html] {
+    def render(in: Unit, key: String, messages: UniformMessages[Html]): Html = Html("")
+  }
 
 }
