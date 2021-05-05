8bed79a1d2bed560882e7ce5f38075d9581bbacc
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-05-05 09:15:04 +0100

    restore CLI and LT interpreters

--- interpreter-logictable/src/main/scala/LogicTableInterpreter.scala
+++ interpreter-logictable/src/main/scala/LogicTableInterpreter.scala
@@ -4,18 +4,36 @@ package interpreters.logictable
 import cats.implicits._
 import ltbs.uniform.validation._
 
-object LogicTableInterpreter extends MonadInterpreter[Either[ErrorTree, +?], SampleData, TellRenderer] {
+object LogicTableInterpreter extends MonadInterpreter[Either[ErrorTree, +?], SampleData, TellRenderer, SampleData] {
 
   def monadInstance = implicitly[cats.Monad[Either[ErrorTree, +?]]]
 
-  override def askImpl[A](key: String, default: Option[A], validation: Rule[A], asker: SampleData[A]): Either[ErrorTree, A] = {
-    validation.either(asker(key).head)
-  }
+  override def askImpl[A](
+    key: String,
+    default: Option[A],
+    validation: Rule[A],
+    customContent: Map[String,(String, List[Any])],
+    asker: SampleData[A]
+  ): Either[ErrorTree, A] = validation.either(asker(key).head)
 
-  override def tellImpl[T](key: String, value: T, teller: TellRenderer[T]):Either[ErrorTree, Unit] =
-    Right(())
+  override def askListImpl[A](
+    key: String,
+    askJourney: (Option[Int], List[A]) => Either[ErrorTree,A],
+    default: Option[List[A]],
+    validation: Rule[List[A]],
+    customContent: Map[String,(String, List[Any])],asker: SampleData[A]
+  ): Either[ErrorTree,List[A]] = validation.either(asker(key))
 
-  override def endImpl(key: String): Either[ErrorTree, Nothing] =
+  override def tellImpl[T](
+    key: String,
+    value: T,
+    customContent: Map[String,(String, List[Any])],
+    teller: TellRenderer[T]
+  ):Either[ErrorTree, Unit] = Right(())
+
+  override def endImpl(
+    key: String,
+    customContent: Map[String,(String, List[Any])]
+  ): Either[ErrorTree,Nothing] =
     Left(ErrorMsg(s"$key (end)").toTree)
-
 }
