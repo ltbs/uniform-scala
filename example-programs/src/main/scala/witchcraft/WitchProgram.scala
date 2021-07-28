333117f68eebf6464bdb294cd90382d4dd4ad196
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-07-28 11:22:14 +0100

    add in length constraints to listing pages

--- example-programs/src/main/scala/witchcraft/WitchProgram.scala
+++ example-programs/src/main/scala/witchcraft/WitchProgram.scala
@@ -6,14 +6,15 @@ import validation._
 package object witchcraft {
 
   def witchProgram = for {
-    _ <- ask[Boolean]("booltest")    
     e <- askList[Evidence]("evidence",
       validation = Rule.forEachInList(Rule.cond({_ != Evidence.HasWartOnNose}, "dont-care-about-warts"))
     ) {
       case (index: Option[Int], existing: List[Evidence]) =>
         ask[Evidence]("ev1", default = index.map(existing))
     }
-    f <- askList[Familiar]("familiars") {
+    f <- askList[Familiar]("familiars",
+      validation = Rule.minLength(1)
+    ) {
       case (index: Option[Int], existing: List[Familiar]) =>
         ask[Familiar]("fam", default = index.map(existing))
     }
