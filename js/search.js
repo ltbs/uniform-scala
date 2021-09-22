9645a6870fe7773a768398748a0bf90f5c5103d8
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2021-09-22 10:48:12 +0100

    updated site

--- js/search.js
+++ js/search.js
@@ -85,7 +85,7 @@ function prepareIdxAndDocMap() {
     {
       "title": "Home",
       "url": "/uniform-scala/",
-      "content": "uniform uniform is a scala Embedded Domain-specific Language (eDSL) for defining, in abstract terms, an interaction with a user. This definition can then be used with one or more interpreters to do useful things such as - provide a Play Framework journey construct a Logic Table showing input and output compile a Javascript-only interface"
+      "content": "uniform Uniform is a scala Embedded Domain-specific Language (eDSL) for defining, in abstract terms, an interaction with a user. This definition can then be used with one or more interpreters to do useful things such as - provide a Play Framework journey construct a Logic Table showing input and output compile a Javascript-only interface"
     } ,    
     {
       "title": "Interacting",
