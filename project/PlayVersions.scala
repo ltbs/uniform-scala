621d399769b46af31bd7445650bc7610e715126d
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2021-10-26 15:19:54 +0100

    removing play 25 support

--- project/PlayVersions.scala
+++ project/PlayVersions.scala
@@ -2,21 +2,6 @@ import sbtcrossproject.Platform
 import sbt._
 import sbt.Keys._
 
-object Play25 extends Platform {
-  def identifier: String = "play25"
-  def sbtSuffix: String = "25"
-  def enable(project: Project): Project = project
-    .settings(
-      libraryDependencies ++= Seq(
-        "com.typesafe.play" %% "play-server"       % "2.5.19" % "provided",
-        "com.typesafe.play" %% "play-omnidoc"      % "2.5.19" % "provided",
-        "com.typesafe.play" %% "play-netty-server" % "2.5.19" % "provided",
-        "com.typesafe.play" %% "play-logback"      % "2.5.19" % "provided",
-        "com.typesafe.play" %% "play-ws"           % "2.5.19" % "provided"
-      )
-    )
-}
-
 object Play26 extends Platform {
   def identifier: String = "play26"
   def sbtSuffix: String = "26"
